2be4fcd8548d7ed57b35884b886153cbb12d86b6
EC2 Default User <ec2-user@ip-172-31-35-53.ap-south-1.compute.internal>
2024-05-31 07:51:44 +0000

    added to git

--- checkstatus.sh
+++ checkstatus.sh
@@ -1,15 +1,12 @@
 var=$(cat /var/lib/jenkins/workspace/NS-GITHUB-JENKINS/output.txt  | grep "High"  | awk '{print $2}' | tail -1)
-
-
 #var=$(cat ./output.txt  | grep "High"  | awk '{print $2}' | tail -1)
-
 
 echo $var
 if [ "$var" -gt "0" ]
 then
-	echo "High Vulnerability Found"
+	echo "High Vulnerability Found...Build will fail"
 	exit 1
 else
-	echo "No High Found"
+	echo "No High Found....build will pass and merge"
 	exit 0
 fi
