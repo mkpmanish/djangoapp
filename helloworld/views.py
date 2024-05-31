2a206f9145d8bbcb832d6096c01d28b6e867a8c1
EC2 Default User <ec2-user@ip-172-31-35-53.ap-south-1.compute.internal>
2024-05-31 16:34:28 +0000

    added to git 27272

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,4 +1,20 @@
 from django.http import HttpResponse
 
+import subprocess
+domain = input("Enter the Domain: ")
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+print(output)
+
+
+
 def index(request):
     return HttpResponse("Hello, world! --- Manish")
+
+
+
+
+def find_user(username):
+    with connection.cursor() as cur:
+        cur.execute(f"""select username from USERS where name = '%s'""" % username)
+        output = cur.fetchone()
+    return output
